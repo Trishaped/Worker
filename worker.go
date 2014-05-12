56664bc6e0e282e3ac28f2323e80f1cf98d54b09
Thomas <github@november-eleven.fr>
2014-05-12 21:32:14 +0200

    Remove switch case test

--- worker.go
+++ worker.go
@@ -6,27 +6,8 @@ import (
 )
 
 func main() {
+
 	fmt.Println("Trishaped worker")
-
-	c := 2
-
-	switch c {
-	case 0:
-		{
-			fmt.Println("x")
-		}
-	case 1:
-		{
-			fmt.Println("y")
-		}
-	case 2:
-		{
-			server.Connect()
-		}
-	default:
-		{
-			fmt.Println("a")
-		}
-	}
+	server.Connect()
 
 }
